5cb3e794fcd016a66985a69bbf1e5a29c2400308
sbrichardson <srichardson@reactual.io>
2019-05-30 23:12:33 -0600

    add babel, packages

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import parseHours from './parseHours'
+require('./parseHours_dev')
 
 export default parseHours
