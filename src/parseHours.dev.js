b0aa114b8bbe8c5c13b30be3f3750ffc6773784e
sbrichardson <srichardson@reactual.io>
2019-05-31 18:14:05 -0600

    add samples

--- src/parseHours.dev.js
+++ src/parseHours.dev.js
@@ -10,6 +10,10 @@ const SAMPLES = [
   'Open ⋅ Closes 12AM · See more hoursThursday6AM–12AMFriday6AM–2AMSaturday6AM–2AMSunday6AM–12AMMonday6AM–12AMTuesday6AM–12AMWednesday6AM–12AMSuggest an editUnable to add this file. Please check that it is a valid photo.',
 
   'Open ⋅ Closes 11PMFriday11AM–11PMSaturday10AM–11PMSunday10AM–10PMMondayClosedTuesdayClosedWednesday11AM–10PMThursday11AM–10PMSuggest an editUnable to add this file. Please check that it is a valid photo.',
+
+  'Open ⋅ Closes 12AMFriday9AM–12AMSaturday9AM–12AMSunday9AM–12AMMonday9AM–12AMTuesday9AM–12AMWednesday9AM–12AMThursday9AM–12AMSuggest an editUnable to add this file. Please check that it is a valid photo.',
+
+  'Open ⋅ Closes 11PM · See more hoursFriday6AM–11PMSaturday6AM–11PMSunday6AM–11PMMonday6AM–11PMTuesday6AM–11PMWednesday6AM–11PMThursday6AM–11PMSuggest an editUnable to add this file. Please check that it is a valid photo.',
 ]
 
 SAMPLES.forEach(x => {
